4c7c5f7aa7b533400bb28c08334f126f3d140836
doutoramon <doutoramon@gmail.com>
2025-03-01 15:51:10 -0300

    manipulacao da classe para informacao do cliente

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+
+        String nomeCliente = "Ramon";
+        double saldo = 237.48;
+
+        System.out.println("Por favor, digite o número da Agência !");
+        int numeroAgencia = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println("Por favor, digite o número da Conta !");
+        String numeroConta = scanner.nextLine();
+        scanner.close();
+
+        System.out.println("Olá "+nomeCliente+", obrigado por criar uma conta em nosso banco, \n" +
+                "sua agência é"+numeroAgencia+", conta "+numeroConta+" e seu \n" +
+                "saldo "+saldo+" já está disponível para saque");
+
+
     }
 }
